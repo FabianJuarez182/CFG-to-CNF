697dc3a7bbff64db5d0229d02a7253bc1de831eb
Mendezg1 <eurocoparich@gmail.com>
2023-10-29 22:22:04 -0600

    fixed spaces

--- main.py
+++ main.py
@@ -8,26 +8,25 @@ from CYKParser import parse_cyk, print_parse_tree
 #T = ['cooks', 'drinks', 'eats', 'cuts', 'he', 'she', 'in', 'with', 'cat', 'dog', 'beer', 'cake', 'juice', 'meat', 'soup', 'fork', 'knife', 'oven', 'spoon', 'a', 'the']
 T = ['a','b']
 # PRODUCCIONES
-'''
+
 P = {
     'N': {'cat', 'dog', 'beer', 'cake', 'juice', 'meat', 'soup', 'fork', 'knife', 'oven', 'spoon'},
     'Det': {'a', 'the'},
     'V': {'cooks', 'drinks', 'eats', 'cuts'},
     'P': {'in', 'with'},
-    'NP': {'he she', 'Det N'}'
+    'NP': {'he she', 'Det N'},
     'PP': {'P NP'},
     'VP': {'VP PP', 'V NP','cooks', 'drinks', 'eats', 'cuts' },
     'A': {'B'},
     'S': {'NP VP A'}
 }
 
-'''
-P = {
-    'S': {'A S B'},
-    'A': {'a A S','a','' },
-    'B': {'S b S','A','b b'},
-    #'AA': {'a'}
-}
+# P = {
+#     'S': {'A S B'},
+#     'A': {'a A S','a','' },
+#     'B': {'S b S','A','b b'},
+#     #'AA': {'a'}
+# }
 
 
 # Lista de no terminales
@@ -39,13 +38,13 @@ S = 'S'
 def main():
     CNF = parseToCNF(T, P, V, S)
     print(CNF)
-    w = "she eats a cake with a fork"
-    parse_tree = parse_cyk(CNF, w)
-    if parse_tree:
-        print("Arbol de parseo:")
-        print_parse_tree(parse_tree)
-    else:
-        print("Expresion invalida")
+    # w = "she eats a cake with a fork"
+    # parse_tree = parse_cyk(CNF, w)
+    # if parse_tree:
+    #     print("Arbol de parseo:")
+    #     print_parse_tree(parse_tree)
+    # else:
+    #     print("Expresion invalida")
     
 
 if __name__ == "__main__":

8e78e04a03be50eb5f8cdb6906ed0d1902f27520
Adrian Fulladolsa <ful21592@uvg.edu.gt>
2023-10-30 21:24:07 -0600

    add runtime to CYK

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from CNFParser import parseToCNF, print_parse_cnf
 from CYKParser import *
+import time
 
 # T, V, S, P
 
@@ -40,7 +41,7 @@ S = 'S'
 EXPRESIONES = [
                 "He cooks the meat with the oven", #Valida y semanticamente correcta
                 "A cat eats the cake", #Valida y semanticamente correcta
-                "The cat eats the oven with a fork", #Valida y semanticamente incorrecta
+                "The cat eats the oven with a fork with the juice in a soup with the meat", #Valida y semanticamente incorrecta
                 "A knife cooks a juice in the dog", #Valida y semanticamente incorrecta
                 "She eats cake with a dog", #Expresion invalida y semanticamente correcta
                 "Cuts soup in an oven" #Expresion invalida y semanticamente incorrecta
@@ -50,14 +51,19 @@ def main():
     CNF = parseToCNF(T, P, V, S)
     print_parse_cnf(CNF)
     for expresion in EXPRESIONES:
+        start_time = time.time()
         w = expresion
         parse_tree = parse_cyk(CNF, w)
+        end_time = time.time()
+        
         if parse_tree:
             print(f"La expresion '{w}' es valida")
+            print(f"Tiempo de ejecucion: {end_time - start_time}")
             print("Arbol de parseo:")
             print_parse_tree(parse_tree)
         else:
             print(f"La expresion '{w}' es invalida")
+            print(f"Tiempo de ejecucion: {end_time - start_time}")
         
     
 
